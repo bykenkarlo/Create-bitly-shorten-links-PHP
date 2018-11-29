712bf657378b0402c133622ad2fc596a5ea56af4
kenkarlo <me@kenkarlo.com>
2018-11-30 01:40:02 +0800

    Adding File

---
+++ bitly.php
@@ -0,0 +1,6 @@
+<?php 
+	$longUrl = 'https://kenkarlo.com';
+	$url  = "http://api.bitly.com/v3/shorten?login=USERNAME&apiKey=API_KEY&longUrl=$longUrl&format=txt";
+	$short_url = @file_get_contents($url); //Bitly will return a short url as text
+	echo $short_url; //display or return our short url
+?>
